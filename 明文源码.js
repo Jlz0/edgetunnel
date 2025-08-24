ee1b39e691748a86fd9e1a8d38740394be823335
Jlz0 <111007912+Jlz0@users.noreply.github.com>
2025-08-24 17:12:27 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,5 +1,5 @@
 import { connect } from 'cloudflare:sockets';
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '';
 let proxyIP = '';
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is not valid');
@@ -446,3 +446,4 @@ clash-meta
 ################################################################
 `;
 }
+
